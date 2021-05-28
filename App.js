485f709e3d261ef185f0ec0b371901b6ab049da3
naufalyukafi10@gmail.com <naufalyukafi10@gmail.com>
2021-05-28 17:31:16 +0700

    change new firebase

--- App.js
+++ App.js
@@ -17,14 +17,14 @@ import Router from './src/router'
 
 const App = () => {
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyACsW7cbakZso75a_01tios6IrazyGRTos",
-    authDomain: "molidu-learning.firebaseapp.com",
-    projectId: "molidu-learning",
-    storageBucket: "molidu-learning.appspot.com",
-    messagingSenderId: "109794889729",
-    appId: "1:109794889729:web:e14530216d38b7e024b000",
-    measurementId: "G-T2Z7SHCJNZ"
+  var firebaseConfig = {
+    apiKey: "AIzaSyCdypIJxkJIhTT8dyLKxfPfht31XkEf0so",
+    authDomain: "molidu-v1.firebaseapp.com",
+    projectId: "molidu-v1",
+    storageBucket: "molidu-v1.appspot.com",
+    messagingSenderId: "736070341526",
+    appId: "1:736070341526:web:4a82a56e20f408f60643b7",
+    measurementId: "G-M8XQPQ3913"
   };
 
   if(!firebase.apps.length) {
